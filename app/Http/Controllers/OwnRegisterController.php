beb6acaa9af256d70f40513258a9bf49957fd33c
kohei0409 <kohei0409@mac.com>
2023-04-03 16:13:41 +0900

    Default Laravel

---
+++ app/Http/Controllers/OwnRegisterController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreOwnRegisterRequest;
+use App\Http\Requests\UpdateOwnRegisterRequest;
+use App\Models\OwnRegister;
+
+class OwnRegisterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOwnRegisterRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOwnRegisterRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OwnRegister  $ownRegister
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OwnRegister $ownRegister)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OwnRegister  $ownRegister
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OwnRegister $ownRegister)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOwnRegisterRequest  $request
+     * @param  \App\Models\OwnRegister  $ownRegister
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOwnRegisterRequest $request, OwnRegister $ownRegister)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OwnRegister  $ownRegister
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OwnRegister $ownRegister)
+    {
+        //
+    }
+}
