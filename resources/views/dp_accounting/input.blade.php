4f878f458cb2fabc981c084863864e5f59c85752
kohei0409 <kohei0409@mac.com>
2023-05-14 18:07:39 +0900

    Default Laravel

---
+++ resources/views/dp_accounting/input.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="py-6 bg-surface-secondary">
+        <div class="container-fluid">
+
+            Accounting.input
+
+
+
+        </div>
+    </main>
+@endsection
